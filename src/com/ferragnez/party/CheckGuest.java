0c0b778716baffa69facd83327ddcefaac834445
matteocovetti <covetti.matteo@gmail.com>
2022-06-10 14:03:30 +0200

    creato ed inizializzato array

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,10 +1,36 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
+	/* creare e inizializzare l’array contenente i nomi degli invitati
+	 * chiedere all’utente come si chiama verificare che il nome sia presente nell’array
+	 * lasciarlo entrare o rispedirlo cortesemente da dove è venuto
+	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		// dichiaro array da 11
+		String[] invitati = new String[11];
+		
+		// assegno valore alle posizioni dell'array
+		invitati[0] = "Dua Lipa";
+		invitati[1] = "Paris Hilton";
+		invitati[2] = "Manuel Agnelli";
+		invitati[3] = "J-Ax";
+		invitati[4] = "Francesco Totti";
+		invitati[5] = "Ilary Blasi";
+		invitati[6] = "Bebe Vio";
+		invitati[7] = "Luis";
+		invitati[8] = "Pardis Zarei";
+		invitati[9] = "Martina Maccherone";
+		invitati[10] = "Rachel Zeilic";
+		
+		scan.close();
+		
 	}
 
 }
